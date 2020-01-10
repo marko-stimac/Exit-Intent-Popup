656e3f6c475a04edb21ea0aab45888f248903336
Marko <filo.veg@gmail.com>
2020-01-10 14:48:18 +0100

    Update author meta info

--- exit-intent-popup.php
+++ exit-intent-popup.php
@@ -1,8 +1,10 @@
 <?php
-/*
-Plugin Name: Exit Intent Popup
-Description: Simple wrapper for popup modal when user intents to leave the website
-Version: 1.0.1
+/**
+ * Plugin Name: Exit Intent Popup
+ * Description: Simple wrapper for popup modal when user intents to leave the website
+ * Version: 1.0.1
+ * Author: Marko Štimac
+ * Author URI: https://marko-stimac.github.io/
 */
 
 namespace ms\ExitIntentPopup;
